25dfc0b9daa320fac1a1feabfe13f121dc88f00c
RogLag <hugo.rog@icloud.com>
2024-11-13 11:32:27 +0100

    ➡️ Added Avatar and Combobox components to home page layout

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -2,13 +2,21 @@
     import { toast } from "svelte-sonner";
     import { Button } from '$lib/components/ui/button';
     import { Badge } from '$lib/components/ui/badge';
+    import * as Avatar from '$lib/components/ui/avatar';
     import Title from '$lib/components/ui/title/Title.svelte';
     import Texte from '$lib/components/ui/texte/Texte.svelte';
     import Informations from '$lib/components/ui/informations/Informations.svelte';
+    import Combobox from "$lib/components/ui/combobox/Combobox.svelte";
 </script>
 
 <div class="w-screen h-screen flex flex-col gap-8 px-48 py-16 bg-noise">
+    <div class="w-full flex items-center justify-between">
     <Title>Neuralab</Title>
+    <Avatar.Root>
+        <Avatar.Image src="https://picsum.photos/200/200" alt="@shadcn" />
+        <Avatar.Fallback>CN</Avatar.Fallback>
+    </Avatar.Root>
+    </div>
     <Texte>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
     </Texte>
@@ -29,4 +37,5 @@
     >
         Show Toast
     </Button>
+    <Combobox data={[{ value: "one", label: "One" }, { value: "two", label: "Two" }]} item="item" />
 </div>
