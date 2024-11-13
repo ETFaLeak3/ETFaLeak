08b0d836bf6e1a5829e13cc5a0e0dca994d7b1eb
RogLag <hugo.rog@icloud.com>
2024-11-13 10:36:55 +0100

    ➡️ Created new home page with Title, Texte, Badge, and Button components; added fade-in animation to layout

---
+++ src/routes/(home)/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { Button } from '$lib/components/ui/button';
+    import { Badge } from '$lib/components/ui/badge';
+    import Title from '$lib/components/ui/title/Title.svelte';
+    import Texte from '$lib/components/ui/texte/Texte.svelte';
+    import Informations from '$lib/components/ui/informations/Informations.svelte';
+</script>
+
+<div class="w-screen h-screen flex flex-col gap-8 px-48 py-16 bg-noise">
+    <Title>Neuralab</Title>
+    <Texte>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </Texte>
+    <Badge>New</Badge>
+    <Button>Click me</Button>
+    <Button variant="outline">Click me</Button>
+    <Informations />
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
 	import '../app.css';
+	import 'animate.css';
+    import { onMount, onDestroy } from "svelte";
+
+    let mounted;
+
+    onMount(() => {
+        setTimeout(() => {
+            mounted = true;
+        }, 100);
+    });
+
+    onDestroy(() => {
+        mounted = false;
+    });
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="animate__animated animate__fadeIn animate__faster">
+    {@render children()}
+</div>
